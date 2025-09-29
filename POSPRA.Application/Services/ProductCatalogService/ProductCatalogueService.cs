cd05290f6a759d1a5f5fc4f99f12bb35c53b75f7
hammad.bokhari@pral.com.pk <hammad.bokhari@pral.com.pk>
2025-09-29 12:20:46 +0500

    Get Product catalog api update

--- POSPRA.Application/Services/ProductCatalogService/ProductCatalogueService.cs
+++ POSPRA.Application/Services/ProductCatalogService/ProductCatalogueService.cs
@@ -1,6 +1,6 @@
 ﻿using AutoMapper;
 using POSPRA.Application.Services.LogService;
-using POSPRA.Domain.Entities;
+using POSPRA.Application.Utility;
 using POSPRA.DTOs;
 using POSPRA.DTOs.ProductCatalogDtos;
 using POSPRA.Repositories.ProductCatalogueRepository;
@@ -24,10 +24,13 @@ namespace POSPRA.Application.Services.ProductCatalogService
 
         public async Task<ApiResponse<List<ProductCatalogueDto>>> GetAllAsync()
         {
-            var output = await _productCatalogueRepository.QueryProcedureAsync<ProductCatalogue>("GetProductCatalogue") ?? new List<ProductCatalogue>();
-            var productCatalogueDTO = _mapper.Map<List<ProductCatalogueDto>>(output) ?? new List<ProductCatalogueDto>();
+            var output = await _productCatalogueRepository.GetAllAsync();
+            var productCatalogueDTO = _mapper.Map<List<ProductCatalogueDto>>(output);
 
-            return new ApiResponse<List<ProductCatalogueDto>>(null, null, productCatalogueDTO, null);
+            if (productCatalogueDTO.Any())
+                return new ApiResponse<List<ProductCatalogueDto>>(ApiStatusCode.Success, ResponseMessages.RecordFound, productCatalogueDTO, string.Empty);
+
+            return new ApiResponse<List<ProductCatalogueDto>>(ApiStatusCode.NotFound, ResponseMessages.DataNotFound, null, string.Empty);
         }
     }
 }
--- POSPRA.DTOs/ProductCatalogDtos/ProductCatalogueDto.cs
+++ POSPRA.DTOs/ProductCatalogDtos/ProductCatalogueDto.cs
@@ -5,8 +5,8 @@
 
         public string? ProductCode { get; set; }
         public string? ProductDescription { get; set; }
-        public string? HSCodes { get; set; }
-        public string? SaleTypes { get; set; }
+        public string? HSCode { get; set; }
+        public string? SaleType { get; set; }
         public string? PosUnitOfMeasurement { get; set; }
         public string? TaxRate { get; set; }
         public string? SroScheduleNumber { get; set; }
