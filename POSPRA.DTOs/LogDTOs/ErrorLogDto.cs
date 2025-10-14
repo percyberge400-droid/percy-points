eaec3ba500b868dcba922ebaccf85700c152545f
asharib.kamal@pral.com.pk <asharib.kamal@pral.com.pk>
2025-10-14 15:04:19 +0500

    Update POSInvoiceReport.rdlc and ErrorLogDto.cs

---
+++ POSPRA.DTOs/LogDTOs/ErrorLogDto.cs
@@ -0,0 +1,15 @@
+﻿namespace POSPRA.DTOs.LogDtos
+{
+    public class ErrorLogDto
+    {
+        public string Message { get; set; } = string.Empty;
+
+        public int POSID { get; set; } = 0;
+
+        public string ActualData { get; set; } = string.Empty;
+
+        public bool IsValidSignature { get; set; } = false;
+
+        public int TotalFiles { get; set; } = 0;
+    }
+}
