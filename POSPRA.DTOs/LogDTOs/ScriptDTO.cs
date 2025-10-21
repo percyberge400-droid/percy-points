377267de49d8ffd33a8b917145d66150b0065867
Syed Hammad Ahmad Bokhari <hammad.bokhari@pral.com.pk>
2025-10-21 12:32:20 +0500

    script DTO

---
+++ POSPRA.DTOs/LogDTOs/ScriptDTO.cs
@@ -0,0 +1,11 @@
+﻿using POSPRA.DTOs.FiscalDtos;
+using POSPRA.DTOs.LogDtos;
+
+namespace POSPRA.DTOs.LogDTOs
+{
+    public class ScriptDTO
+    {
+        public List<LogDto>? Log { get; set; }
+        public List<FileRecordDto>? FileRecord { get; set; }
+    }
+}
