c63dd75cffe8b411e69c56a92f78259961b000bd
Syed Hammad Ahmad Bokhari <hammad.bokhari@pral.com.pk>
2025-11-18 15:29:57 +0500

    fix product catalogue api get from Production db

--- Pos.Infrastructure/Persistence/Repositories/ProductCatalogueByPosIdRepository.cs
+++ Pos.Infrastructure/Persistence/Repositories/ProductCatalogueByPosIdRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Pos.Application.DTOs.ProductCatalogDtos;
 using Pos.Application.Interfaces;
 using Pos.Application.Interfaces.ProductCatalogue;
@@ -10,23 +11,36 @@ namespace Pos.Infrastructure.Persistence.Repositories.ProductCatalogue
         private readonly IRepository<Pos.Domain.Entities.ProductCatalogue> _sqlProductCatalogueRepository;
         private readonly IRepository<Pos.Domain.Entities.PosClients> _sqlPosClientRepository;
         private readonly IRepository<Pos.Domain.Entities.POSBranches> _sqlPosBranchesRepository;
+        private readonly IConfiguration _configuration;
 
         public ProductCatalogueByPosIdRepository(
-            ISqlServerRepositoryFactory sqlRepositoryFactory)
+            ISqlServerRepositoryFactory sqlRepositoryFactory, IConfiguration configuration)
         {
             // Get the DbContext from the factory or inject it directly
             _sqlProductCatalogueRepository = sqlRepositoryFactory.CreateRepository<Pos.Domain.Entities.ProductCatalogue>();
             _sqlPosClientRepository = sqlRepositoryFactory.CreateRepository<Pos.Domain.Entities.PosClients>();
             _sqlPosBranchesRepository = sqlRepositoryFactory.CreateRepository<Pos.Domain.Entities.POSBranches>();
+            _configuration = configuration;
         }
 
         public async Task<IEnumerable<ProductCatalogueDto>> GetProductCatalogueByPosIdAsync(int posId)
         {
+            // Get the production connection string
+            var connectionString = _configuration.GetConnectionString("ProductionConnection");
+
+            // Create DbContext options dynamically
+            var optionsBuilder = new DbContextOptionsBuilder<SqlServerDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            // Use a temporary DbContext
+            await using var dbContext = new SqlServerDbContext(optionsBuilder.Options);
+
+            // Query using the temporary DbContext
             var output =
-                from client in _sqlPosClientRepository.Query()
-                join branch in _sqlPosBranchesRepository.Query()
+                from client in dbContext.PosClients
+                join branch in dbContext.POSBranches
                     on client.POSBranchID equals branch.POSBranchID
-                join pc in _sqlProductCatalogueRepository.Query()
+                join pc in dbContext.ProductCatalogue
                     on branch.POSMASTERID equals pc.POSMasterId
                 where client.POSRegistrationNumber == posId
                 select new ProductCatalogueDto
@@ -44,5 +58,6 @@ namespace Pos.Infrastructure.Persistence.Repositories.ProductCatalogue
 
             return await output.ToListAsync();
         }
+
     }
 }
