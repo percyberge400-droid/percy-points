3cc7e3187a5056274cbccc6402463b7e5dd2b637
Muhammad Talha Arif <talha.arif@pral.com.pk>
2025-12-19 11:48:51 +0500

    updated urls

--- Pos.WinFormsUI/Forms/loginForm2.cs
+++ Pos.WinFormsUI/Forms/loginForm2.cs
@@ -550,7 +550,7 @@ namespace Pos.WinFormsUI.Forms
             Log($"Local version: {localVersion}");
 
             // ---------------- Call API ----------------
-            string apiBase = "http://10.105.200.161/live/Configuration/";
+            string apiBase = "http://10.105.200.161/api/live/";
             string versionApi = $"{apiBase}get-update-version";
 
             string serverVersion;
